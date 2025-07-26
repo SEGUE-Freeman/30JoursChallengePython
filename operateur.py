96b3bd9ad8e6c816298d9a30c48a4fe65d8da32e
SEGUE-Freeman <syfl.segue@gmail.com>
2025-07-26 00:42:51 +0000

    Add files via upload

---
+++ operateur.py
@@ -0,0 +1,50 @@
+age = 18
+taille = 1.75
+nombre_complexes = 3 + 2j
+
+#4)
+base = int(input("Entrez la base : "))
+hauteur = input("Entrez le nom de l'auteur : ")
+zone = 0.5 * base * hauteur
+
+print("La zone du triangle est :", zone)
+
+#5)
+A , B, C = int(input("Entrez le premier côté : ")), int(input("Entrez le deuxième côté : ")), int(input("Entrez le troisième côté : "))
+perimetre_triangle = A + B + C
+print("Le périmètre du triangle est :", perimetre_triangle)
+
+#6)
+longueur = int(input("Entrez la longueur du rectangle : "))
+largeur = int(input("Entrez la largeur du rectangle : "))
+surface_rectangle = longueur * largeur
+perimetre_rectangle = 2 * (longueur + largeur)
+print("La surface du rectangle est :", surface_rectangle)
+print("Le périmètre du rectangle est :", perimetre_rectangle)
+
+#7)
+rayon = int(input("Entrez le rayon du cercle : "))
+pi = 3.14
+zone_cercle = pi * rayon ** 2
+circonference_cercle = 2 * pi * rayon
+print("La zone du cercle est :", zone_cercle)
+print("La circonférence du cercle est :", circonference_cercle)
+
+#8)
+y = 2*x - 2
+print("pente de la droite :", 2)
+print("ordonnée à l'origine de la droite :", -2)
+
+#9)
+pente = (y_2 - y_1) / (x_2 - x_1)
+x_1 = 2
+x_2 = 6
+y_1 = 2
+y_2 = 10
+
+print("La pente de la droite est :", pente)
+
+dstance = ((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2) ** 0.5
+print("La distance entre les points est :", dstance)
+
+# 10)comparaison des pentes du numero 
